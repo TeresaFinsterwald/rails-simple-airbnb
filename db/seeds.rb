d43054fe5c8d20ae11c5be3a8843fe171aa01c98
Teresa Finsterwald <teresa.finsterwald@gmail.com>
2021-11-10 16:43:01 +0100

    simple airbnb

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,20 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+puts "destroying flats"
+Flat.destroy_all
+puts "creating flats"
+
+4.times do
+  flat = Flat.new(
+    name: Faker::Tea.variety,
+    address: Faker::Address.street_name,
+    description: Faker::ChuckNorris.fact,
+    price_per_night: rand(0..1000),
+    number_of_guests: rand(0..8)
+  )
+  flat.save
+end
+
+puts "#{Flat.count} flats were created"
